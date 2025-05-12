5f01f2f0294cc4d9234755cf5c5a5203251d2caf
BrendanH-Programmer <brendanhenderson@live.co.uk>
2025-05-12 12:36:57 +0100

    variables order change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
-  features {}
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
+  features        = {}
 }
 
 resource "azurerm_resource_group" "example" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,27 @@
 variable "client_id" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "client_secret" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "tenant_id" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "subscription_id" {
-  type = string
+  type    = string
+  default = ""
+}
+
+provider "azurerm" {
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  features        = {}
 }
