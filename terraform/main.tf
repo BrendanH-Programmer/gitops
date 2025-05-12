3c134be39a3651830dbf91ba37666e5dcf649202
BrendanH-Programmer <brendanhenderson@live.co.uk>
2025-05-12 12:03:42 +0100

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "UK SOUTH"
