a236320bde971473ad74632e2a3f8c6f5d723380
BrendanH-Programmer <brendanhenderson@live.co.uk>
2025-05-12 12:41:19 +0100

    duplication

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,11 +17,3 @@ variable "subscription_id" {
   type    = string
   default = ""
 }
-
-provider "azurerm" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  features        = {}
-}
