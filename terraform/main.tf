70b449c6d44d14fdd9b0880c16dadef3a3d96b92
BrendanH-Programmer <brendanhenderson@live.co.uk>
2025-05-12 12:44:35 +0100

    feature not expected removed

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,6 @@ provider "azurerm" {
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
-  features        = {}
 }
 
 resource "azurerm_resource_group" "example" {
